ef5653cb19dbb7cf3a283d922883b3f5baa35fd5
Asad Hasan <asad@dadesigners.com>
2015-07-15 12:07:19 -0500

    Setup new passport defaults.

--- config/policies.js
+++ config/policies.js
@@ -19,6 +19,11 @@
 
 module.exports.policies = {
 
+  '*': ['passport', 'sessionAuth'],
+  'auth': {
+    '*': ['passport']
+  }
+
   /***************************************************************************
   *                                                                          *
   * Default policy for all controllers and actions (`true` allows public     *
