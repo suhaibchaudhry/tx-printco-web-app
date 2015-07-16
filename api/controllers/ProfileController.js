653d4d0aa68c8b4d00e7733e349274e7ed979fd5
Asad Hasan <asad@dadesigners.com>
2015-07-15 20:15:18 -0500

    Create profile controller.

---
+++ api/controllers/ProfileController.js
@@ -0,0 +1,14 @@
+/**
+ * ProfileController
+ *
+ * @description :: Server-side logic for managing Profiles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  profile: function (req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  }
+};
