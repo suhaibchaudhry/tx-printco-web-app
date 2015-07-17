bc31bdd09d68d30532dd364447713af111e56725
Asad Hasan <asad@dadesigners.com>
2015-07-17 17:44:28 -0500

    Introduce make request method, move helper app object in to seperate file.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,38 @@
 (function($) {
-  App = {};
-  App.View = {
-      collection: [],
-      Bootstrap: function(viewClass, selector) {
-        var view = new viewClass({
-          el: $(selector).get(0)
-        });
+  App = {
+      basePath: '/',
+      makeRequest: function(resource, requestType, contextObj, sucessCB, errorCB, data) {
+        var request = {
+          type: requestType,
+          url: this.basePath+resource,
+          success: _.bind(successCB, contextObj),
+          error: _.bind(errorCB, contextObj)
+        };
 
-        App.View.collection.push(view);
-        return view;
+        if(typeof success == 'function') {
+          request['success'] =  _.bind(successCB, contextObj);
+        }
+
+        if(typeof error == 'function') {
+          request['error'] =  _.bind(errorCB, contextObj);
+        }
+
+        if(typeof data == 'object') {
+          request['data'] = data;
+        }
+
+        $.ajax(request);
+      },
+      View: {
+          collection: [],
+          Bootstrap: function(viewClass, selector) {
+            var view = new viewClass({
+              el: $(selector).get(0)
+            });
+
+            App.View.collection.push(view);
+            return view;
+          }
       }
   };
-
-  var applicationFrame = Backbone.View.extend({
-    tagName: 'div',
-    events: {
-
-    },
-    initialize: function() {
-      console.log("Initialized");
-    },
-    render: function() {
-      console.log("Rendered");
-    }
-  });
-
-  var view = App.View.Bootstrap(applicationFrame, 'div.app-wrap');
 })(jQuery);
--- assets/js/product-filters.js
+++ assets/js/product-filters.js
@@ -2,13 +2,28 @@
   var productFilters = Backbone.View.extend({
     tagName: 'div',
     events: {
-
+      "change .product-filters select": 'lookupProduct'
     },
     initialize: function() {
       console.log("Initialized");
     },
     render: function() {
       console.log("Rendered");
+    },
+    lookupProduct: function(e) {
+      console.log(this.$('.product-filters select').val());
+      $.ajax({
+        type: 'POST',
+        url: App.basePath+'rpc/product/filter',
+
+        timeout: 1500,
+        success: function(res, status, xhr) {
+
+        },
+        error: function() {
+
+        }
+      });
     }
   });
 
