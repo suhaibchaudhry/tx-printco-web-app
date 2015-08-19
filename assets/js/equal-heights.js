248f704c572ec0e61d7598d74b1543f238c95f3f
Asad Hasan <asad@dadesigners.com>
2015-08-19 15:20:38 -0500

    Add slideshow animation.

--- assets/js/equal-heights.js
+++ assets/js/equal-heights.js
@@ -1,4 +1,23 @@
 (function($) {
 	$('.footer .menu').equalHeights();
 	$('.middle-content .block').equalHeights();
+
+	//Slideshow, Move in to its own file later.
+	var $slideshow = $('.slideshow');
+	$slideshow.find('.menu a').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	  var next_slide = $(e.currentTarget.hash);
+		var current_slide = $('.slideshow .active-slide');
+		if(!next_slide.hasClass('active-slide') && current_slide.length == 1) {
+			next_slide.addClass('active-slide');
+			current_slide.addClass('shifted-slide');
+			next_slide.addClass('shifted-slide');
+			setTimeout(function() {
+				current_slide.removeClass('active-slide').removeClass('shifted-slide');
+				next_slide.removeClass('shifted-slide');
+				$slideshow.prepend(next_slide);
+			}, 1000);
+		}
+	});
 })(jQuery);
