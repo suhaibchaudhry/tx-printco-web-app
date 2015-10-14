87b7fe6bfd26802eeeacec479fbe3a43d68b9185
Asad Hasan <asad@dadesigners.com>
2015-10-13 19:00:14 -0500

    Recreate custom action.

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -5,7 +5,10 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
-
+ module.exports = {
+ 	orderProfileData: function(req, res) {
+     res.json({
+       authenticated: Boolean(req.session.authenticated)
+     });
+   }
+ };
