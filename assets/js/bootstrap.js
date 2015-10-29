60d7cb94e35bf1e4186e92858c607ef8f2d2706f
Asad Hasan <asad@dadesigners.com>
2015-10-29 13:52:55 -0500

    Override error message handling.

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,3 +1,21 @@
+//Configure DropZone
+Dropzone.options.fileUploader = {
+  init: function() {
+       this.on('error', function(file, response) {
+           // (assuming your response object has an errorMessage property...)
+           var errorMessage = 'An error occured while uploading.';
+           if(_.has(response, 'errorMessage')) {
+             errorMessage = response.errorMessage;
+           } else if(_.has(response, 'message')) {
+             errorMessage = response.message;
+           }
+
+           $(file.previewElement).find('.dz-error-message').text(errorMessage);
+       });
+   }
+};
+
+//Bootstrap App
 (function($) {
   $(document).ready(function() {
     App.View.RenderAll();
